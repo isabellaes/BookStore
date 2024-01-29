ec3cfbca6d1f2afb003ebccbba289b8216ea49b5
isabellaes <borjessonisabella@gmail.com>
2024-01-29 19:30:03 +0100

    update checkout

--- client/src/pages/CheckoutPage.tsx
+++ client/src/pages/CheckoutPage.tsx
@@ -71,8 +71,6 @@ const CheckoutPage: React.FC = () => {
   }
   return (
     <div className="container-checkout">
-      <h2>Checkout</h2>
-
       <Breadcrumbs aria-label="breadcrumb">
         <Link underline="hover" color="inherit" href="/">
           Home
@@ -84,78 +82,85 @@ const CheckoutPage: React.FC = () => {
           Checkout
         </Link>
       </Breadcrumbs>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.product.id}>
+      <h2>Checkout</h2>
+
+      {cartItems.map((item) => (
+        <div key={item.product.id} className="cart-item">
+          <div className="left">
+            <img src={item.product.img} alt="" />
+          </div>
+          <div className="rigth">
             {item.product.name} - ${item.product.price.toFixed(2)} - Quantity:{" "}
             {item.quantity}
-          </li>
-        ))}
-        <p>Total: ${totalCost.toFixed(2)}</p>
-      </ul>
-      <p>Shipping and payment</p>
-      <form onSubmit={onsubmit}>
-        <label htmlFor="FirstName">FirstName:</label>
-        <input
-          type="text"
-          id="FirstName"
-          placeholder="FirstName"
-          onChange={(e) => {
-            setFirstName(e.target.value);
-          }}
-        />
-        <label htmlFor="LastName">FirstName:</label>
-        <input
-          type="text"
-          id="LastName"
-          placeholder="LastName"
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <label htmlFor="Adress">Adress:</label>
-        <input
-          type="text"
-          id="Adress"
-          placeholder="Adress"
-          onChange={(e) => setAdress(e.target.value)}
-        />
-        <label htmlFor="ZipCode">Zipcode:</label>
-        <input
-          type="text"
-          id="ZipCode"
-          placeholder="ZipCode"
-          onChange={(e) => setZipCode(e.target.value)}
-        />
-        <label htmlFor="City">City:</label>
-        <input
-          type="text"
-          id="City"
-          placeholder="City"
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <label htmlFor="PhoneNumber">PhoneNumber:</label>
-        <input
-          type="text"
-          id="PhoneNumber"
-          placeholder="PhoneNumber"
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-        <label htmlFor="Email">Email:</label>
-        <input
-          type="text"
-          id="Email"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <label htmlFor="Card">CardNumber</label>
-        <input
-          type="text"
-          id="Card"
-          placeholder="CardNumber"
-          onChange={(e) => setCardNumber(e.target.value)}
-        />
+          </div>
+        </div>
+      ))}
+      <p>Total: ${totalCost.toFixed(2)}</p>
+      <div className="shipping">
+        <h3>Shipping and payment</h3>
+        <form onSubmit={onsubmit}>
+          <label htmlFor="FirstName">FirstName:</label>
+          <input
+            type="text"
+            id="FirstName"
+            placeholder="FirstName"
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+          <label htmlFor="LastName">FirstName:</label>
+          <input
+            type="text"
+            id="LastName"
+            placeholder="LastName"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <label htmlFor="Adress">Adress:</label>
+          <input
+            type="text"
+            id="Adress"
+            placeholder="Adress"
+            onChange={(e) => setAdress(e.target.value)}
+          />
+          <label htmlFor="ZipCode">Zipcode:</label>
+          <input
+            type="text"
+            id="ZipCode"
+            placeholder="ZipCode"
+            onChange={(e) => setZipCode(e.target.value)}
+          />
+          <label htmlFor="City">City:</label>
+          <input
+            type="text"
+            id="City"
+            placeholder="City"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <label htmlFor="PhoneNumber">PhoneNumber:</label>
+          <input
+            type="text"
+            id="PhoneNumber"
+            placeholder="PhoneNumber"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <label htmlFor="Email">Email:</label>
+          <input
+            type="text"
+            id="Email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="Card">CardNumber</label>
+          <input
+            type="text"
+            id="Card"
+            placeholder="CardNumber"
+            onChange={(e) => setCardNumber(e.target.value)}
+          />
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
